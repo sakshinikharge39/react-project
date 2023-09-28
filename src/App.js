b04893d080711229647cd86dc7cd1c6ce660fd5f
sakshinikharge39 <srnikharge7788@gmail.com>
2023-09-28 20:05:15 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./assets/css/style.css";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Menu from './components/Menu';
+const App = () => {
+  return (
+  <>
+    <Navbar />
+    <Home />
+    <About />
+    <Menu />
+  </>
+  );
+};
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return <>
+    <section className='home' id='home'>
+        <div className='content'>
+            <h3>fresh <span>food in the </span>morning</h3>
+            <p>lorem ipsum dolor sit amet, consectutor adipising elit. placeit labore
+                , sint cupididate distincito tempora reiciendis
+            </p>
+            <a href='#' className='btn'>Get Yours Now</a>           
+            </div>
+    </section>
+  </>;
+};
+
+export default Home;
